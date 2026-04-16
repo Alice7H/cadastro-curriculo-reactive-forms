1c7332ca94185484ab438e5cdd70cd2b6fed77c9
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-16 17:08:15 -0300

    feat: update validation directive with dynamic styling for valid and invalid states

--- src/app/shared/directives/input-validation-directive.ts
+++ src/app/shared/directives/input-validation-directive.ts
@@ -1,4 +1,4 @@
-import { DestroyRef, Directive, HostListener, inject, Input } from "@angular/core";
+import { DestroyRef, Directive, ElementRef, HostListener, inject, Input, Renderer2 } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { NgControl } from "@angular/forms";
 
@@ -8,6 +8,8 @@ import { NgControl } from "@angular/forms";
 export class InputValidationDirective {
   private readonly _ngControl = inject(NgControl, {self: true});
   private readonly _destroyRef = inject(DestroyRef);
+  private readonly _elementRef = inject(ElementRef);
+  private readonly _renderer2 = inject(Renderer2);
 
   @Input() errorMessage = 'Campo inválido.';
 
@@ -32,9 +34,34 @@ export class InputValidationDirective {
 
     const isInvalid = control.invalid && control.touched;
     if(isInvalid){
-      console.log('invalido');
+      this.setInvalidStyles();
     } else {
-      console.log('válido');
+      this.setValidStyles();
     }
   }
+ 
+  setInvalidStyles() {
+    const element = this._elementRef.nativeElement;
+
+    this._renderer2.removeClass(element, 'border-zinc-800');
+    this._renderer2.removeClass(element, 'focus:border-violet-500');
+    this._renderer2.removeClass(element, 'focus:ring-violet-500');
+
+    this._renderer2.addClass(element, 'border-red-500');
+    this._renderer2.addClass(element, 'focus:border-red-500');
+    this._renderer2.addClass(element, 'focus:ring-red-500');
+  }
+
+  setValidStyles() {
+    const element = this._elementRef.nativeElement;
+
+    this._renderer2.removeClass(element, 'border-red-500');
+    this._renderer2.removeClass(element, 'focus:border-red-500');
+    this._renderer2.removeClass(element, 'focus:ring-red-500');
+
+    this._renderer2.addClass(element, 'border-zinc-800');
+    this._renderer2.addClass(element, 'focus:border-violet-500');
+    this._renderer2.addClass(element, 'focus:ring-violet-500');
+  }
+
 }
