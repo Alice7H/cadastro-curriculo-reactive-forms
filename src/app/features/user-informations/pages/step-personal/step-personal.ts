878cbb302c79335a9b6e003164c94d529ee5b94b
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-16 11:16:30 -0300

    feat: update personal form submission to use router navigation

--- src/app/features/user-informations/pages/step-personal/step-personal.ts
+++ src/app/features/user-informations/pages/step-personal/step-personal.ts
@@ -1,21 +1,23 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { CurriculumFormStore } from '../../../../core/services/curriculum-form-store';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-step-personal',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './step-personal.html',
 })
 export class StepPersonal {
   readonly curriculumFormStore = inject(CurriculumFormStore);
+  readonly router = inject(Router)
   
   ngOnInit(){
     console.log('Personal group ', this.curriculumFormStore.personalFormGroup)
   }
 
-  logPersonal(){
+  goToProfessional(){
     console.log('Personal value: ', this.curriculumFormStore.personalFormGroup.value)
+    this.router.navigate(['/professional']);
   }
 }
