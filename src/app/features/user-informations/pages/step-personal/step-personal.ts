cf49fb90121129a3b8bd7eca2067ade4dc7caf40
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-16 14:07:34 -0300

    feat: integrate API to load states in StepPersonal

--- src/app/features/user-informations/pages/step-personal/step-personal.ts
+++ src/app/features/user-informations/pages/step-personal/step-personal.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CurriculumFormStore } from '../../../../core/services/curriculum-form-store';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StatesAndCitiesApi } from '../../../../core/services/states-and-cities-api';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-step-personal',
@@ -10,14 +12,26 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class StepPersonal {
   readonly curriculumFormStore = inject(CurriculumFormStore);
-  readonly router = inject(Router)
+  private readonly _statesAndCitiesApi = inject(StatesAndCitiesApi);
+  private readonly _router = inject(Router)
   
+  statesResource = rxResource({
+    params: () => true,
+    stream: () => this._statesAndCitiesApi.getStates(),
+  });
+
+  statesList = computed(()=> {
+    const ERROR_ON_RESPONSE = !!this.statesResource.error();
+    if(ERROR_ON_RESPONSE) return [];
+    return this.statesResource.value() ?? [];
+  })
+
   ngOnInit(){
     console.log('Personal group ', this.curriculumFormStore.personalFormGroup)
   }
 
   goToProfessional(){
     console.log('Personal value: ', this.curriculumFormStore.personalFormGroup.value)
-    this.router.navigate(['/professional']);
+    this._router.navigate(['/professional']);
   }
 }
