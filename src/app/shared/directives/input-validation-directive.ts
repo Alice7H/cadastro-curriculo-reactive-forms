4c0b1e5f12014e2da2208bdf5be6a43044dd6993
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-16 16:45:13 -0300

    feat: update validation directive to handle statusChanges and access FormControl

--- src/app/shared/directives/input-validation-directive.ts
+++ src/app/shared/directives/input-validation-directive.ts
@@ -1,17 +1,40 @@
-import { Directive, HostListener, Input } from "@angular/core";
+import { DestroyRef, Directive, HostListener, inject, Input } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { NgControl } from "@angular/forms";
 
 @Directive({
   selector: '[appInputValidation]',
 })
 export class InputValidationDirective {
+  private readonly _ngControl = inject(NgControl, {self: true});
+  private readonly _destroyRef = inject(DestroyRef);
+
   @Input() errorMessage = 'Campo inválido.';
 
   ngOnInit(){
     console.log('onInit error message: ', this.errorMessage);
+    this._ngControl.statusChanges?.pipe(takeUntilDestroyed(this._destroyRef))
+    .subscribe(()=> {
+      console.log('status mudou!');
+      this.updateStatus();
+    })
   }
 
   @HostListener('blur')
   onBlur(){
     console.log('Ocorreu um blur');
+    this.updateStatus();
+  }
+
+  updateStatus() {
+    const control = this._ngControl.control;
+    if(!control) return;
+
+    const isInvalid = control.invalid && control.touched;
+    if(isInvalid){
+      console.log('invalido');
+    } else {
+      console.log('válido');
+    }
   }
 }
